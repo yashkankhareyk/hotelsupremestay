f744555c43d09e8c723409d9b263029e547fa1b5
yashjayale <135498547+yashjayale@users.noreply.github.com>
2026-01-17 12:36:23 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,7 +18,7 @@ export default function About() {
       <section className="relative h-[50vh] sm:h-[60vh] md:h-96 flex items-center justify-center">
         <div className="absolute inset-0 z-0">
           <img
-            src="https://res.cloudinary.com/dnocdufwt/image/upload/v1765350451/gallery/zpwcoeswsawj0swafjn3.webp"
+            src="https://res.cloudinary.com/dnocdufwt/image/upload/v1768633554/DSC01578_nkaah3.jpg"
             alt="About Sunshine Hotel"
             className="w-full h-full object-cover"
           />
